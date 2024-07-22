6f9dfa31be75826ed0dad08f48fa26f949aeb3d7
Minouccc <343723899@qq.com>
2024-07-22 17:29:58 +0800

    fix: checkout cdn

--- app/PdfDownload.tsx
+++ app/PdfDownload.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { useEffect, useState } from "react";
 import { Document, Page } from "react-pdf";
-import { pdfjs } from "react-pdf";
 import { PDFDocument, degrees } from "pdf-lib";
+// import { pdfjs } from "react-pdf";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   "pdfjs-dist/build/pdf.worker.min.mjs",
+//   import.meta.url
+// ).toString();
+import { pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 export function PdfDownload() {
   const [file, setFile] = useState<File | null>(null);
